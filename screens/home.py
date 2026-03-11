e2c22c8912030be0375cca7e246cea69fc70f716
Javohir <qobiljonsharipov33@gmail.com>
2026-03-11 20:31:43 +0500

    Qlabel alignment changed to cneter

--- screens/home.py
+++ screens/home.py
@@ -1,14 +1,17 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import Qt
 from utils.style import readStyles
+
 
 class HomeScreen(QWidget):
     def __init__(self):
         super().__init__()
-        
+
         layout = QVBoxLayout()
-        
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
         label = QLabel("Undefined name")
         layout.addWidget(label)
-        
+
         self.setLayout(layout)
         self.setStyleSheet(readStyles("screens/homeScreen.css"))
