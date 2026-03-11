d776c874b71d83441f1928aaf6c6c7c827c355fb
Javohir <qobiljonsharipov33@gmail.com>
2026-03-11 20:17:16 +0500

    Added home screen on main window

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget
+from screens.home import HomeScreen
+from PyQt5.QtCore import QSize
 
 from utils.style import readStyles
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("QAPP")
+        
+        self.pages = QStackedWidget()
+        self.home = HomeScreen()
+        
+        self.pages.addWidget(self.home)
+        self.setCentralWidget(self.pages)
+        
         self.setStyleSheet(readStyles("mainWindow.css"))
-
+        self.setFixedSize(QSize(600, 600))
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
-
 app.exec()
