d648315d642ac88c3f13983a53f1bbe82547c944
Javohir <qobiljonsharipov33@gmail.com>
2026-03-11 19:58:16 +0500

    Writed helper style read function for real css styles

---
+++ utils/style.py
@@ -0,0 +1,7 @@
+def readStyles(file_path) -> str:
+    try:
+        with open(f"styles/{file_path}", "r") as style:
+            return style.read()
+    except Exception as e:
+        raise e
+        
