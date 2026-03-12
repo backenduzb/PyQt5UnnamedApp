0b4fe484d2cf3b29a9726253c2ffe621f878734b
Javohir <qobiljonsharipov33@gmail.com>
2026-03-12 13:34:26 +0500

    Scroll areas border was removed for telegram style

--- screens/mainWindow.py
+++ screens/mainWindow.py
@@ -1,5 +1,9 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QLabel, QScrollArea, QSizePolicy, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import (
+    QFrame, QLabel, QScrollArea,
+    QSizePolicy, QVBoxLayout,
+    QWidget,
+)
 
 from components.chatMenu import ChatMenu
 from utils.style import readStyles
@@ -19,13 +23,15 @@ class HomeScreen(QWidget):
         layout.addWidget(label, alignment=Qt.AlignmentFlag.AlignRight)
 
         scroll = QScrollArea()
-        scroll.setWidgetResizable(False)
+        scroll.setWidgetResizable(True)
+        scroll.setFrameShape(QFrame.Shape.NoFrame)
+        
         chat_menu = ChatMenu()
-        chat_menu.setFixedWidth(300)
-        chat_menu.setFixedHeight(800)
         scroll.setWidget(chat_menu)
         scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         scroll.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Expanding)
+        
+        layout.setContentsMargins(0, 0, 0, 0) 
         layout.addWidget(scroll, alignment=Qt.AlignmentFlag.AlignLeft)
 
         self.setLayout(layout)
