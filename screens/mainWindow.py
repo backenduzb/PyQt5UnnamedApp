85a42eace9af8458e4cf7294076bc0e5331b1596
Javohir <qobiljonsharipov33@gmail.com>
2026-03-12 13:08:50 +0500

    Changed main window settings

---
+++ screens/mainWindow.py
@@ -0,0 +1,32 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel, QScrollArea, QSizePolicy, QVBoxLayout, QWidget
+
+from components.chatMenu import ChatMenu
+from utils.style import readStyles
+
+
+class HomeScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+
+        label = QLabel("Welcome")
+        label.setAlignment(Qt.AlignmentFlag.AlignRight)
+        font = label.font()
+        font.setPointSize(14)
+        label.setFont(font)
+        layout.addWidget(label, alignment=Qt.AlignmentFlag.AlignRight)
+
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(False)
+        chat_menu = ChatMenu()
+        chat_menu.setFixedWidth(300)
+        chat_menu.setFixedHeight(800)
+        scroll.setWidget(chat_menu)
+        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        scroll.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Expanding)
+        layout.addWidget(scroll, alignment=Qt.AlignmentFlag.AlignLeft)
+
+        self.setLayout(layout)
+        self.setStyleSheet(readStyles("screens/homeScreen.css"))
