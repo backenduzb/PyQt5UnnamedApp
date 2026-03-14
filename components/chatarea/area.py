578d02aea863d1a775b1781e85c3d3ce80109dc4
Javohir <qobiljonsharipov33@gmail.com>
2026-03-14 19:39:56 +0500

    Addded chat message container and gived telegram styles but border not seeing as telegram message container

--- components/chatarea/area.py
+++ components/chatarea/area.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QListWidget, QListWidgetItem, QAbstractItemView
 from utils.style import readStyles
+from .messagebox import MessageBox
 
 class ChatArea(QWidget):
     def __init__(self):
@@ -8,11 +9,27 @@ class ChatArea(QWidget):
         self.chatList = QListWidget()
         self.chatList.setObjectName("chatArea")
         self.chatList.setContentsMargins(0, 0, 0, 0)
-        self.chatList.setEnabled(False)
-        self.chatList.setSelectionMode(QAbstractItemView.NoSelection)
-        for i in range(20):
-            item = QListWidgetItem("Salom")
+        self.chatList.setVerticalScrollMode(QAbstractItemView.ScrollPerPixel)
+        self.chatList.verticalScrollBar().setSingleStep(8)
+        # self.chatList.setEnabled(False)
+        # self.chatList.setSelectionMode(QAbstractItemView.NoSelection)
+        
+        for i in range(10):
+            item = QListWidgetItem()
+            message_box = MessageBox(f"Test uchun bu habarni jonatdim {i}", this_me=True)
+            
+            item.setSizeHint(message_box.sizeHint())
             self.chatList.addItem(item)
+            self.chatList.setItemWidget(item, message_box)
+        
+        for i in range(10):
+            item = QListWidgetItem()
+            message_box = MessageBox(f"Men ham buin test uchun jonatdim menimcha hozircha yaxshi ishlayapti {i}, ajoyib hozircha message boxlar tog;ri ishlayapti endi kattaroq matnlar bilan test qilish kerak", this_me=False)
+            
+            item.setSizeHint(message_box.sizeHint())
+            self.chatList.addItem(item)
+            self.chatList.setItemWidget(item, message_box)
+        
         
         layout = QHBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
--- components/chatarea/messagebox.py
+++ components/chatarea/messagebox.py
@@ -1,16 +1,29 @@
 from PyQt5.QtWidgets import QLabel, QWidget, QHBoxLayout
+from PyQt5.QtCore import Qt
 from utils.style import readStyles
 
 class MessageBox(QWidget):
-    def __init__(self, message):
+    def __init__(self, message: str, this_me: bool):
         super().__init__()
         
         layout = QHBoxLayout()
-        message = QLabel(message)
         
+        message_label = QLabel(message)
+        message_label.setObjectName("message-text" if this_me else "message-text-other")
         
-        layout.addWidget(message)
-        self.setMinimumHeight(10)
-        self.setMinimumWidth(80)
+        message_label.setWordWrap(True)
+        message_label.setMaximumWidth(800)
+        
+        if this_me:
+            layout.setAlignment(Qt.AlignRight)
+            layout.setContentsMargins(1, 1, 1, 1)
+        else:
+            layout.setAlignment(Qt.AlignLeft)
+            layout.setContentsMargins(1, 1, 1, 1)
+        
+        layout.addWidget(message_label)
+        
         self.setLayout(layout)
         self.setStyleSheet(readStyles("components/chatarea/messageBox.css"))
+        self.setSizePolicy(message_label.sizePolicy())
+        self.setMinimumHeight(30)
